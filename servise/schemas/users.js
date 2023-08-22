6cf6e19aa0bea4ab2a5ecc4d5ea85130b122fb7d
Prymushko Oleh <oleg.prim70p@gmail.com>
2023-08-22 16:11:03 +0300

    avatarURL

--- servise/schemas/users.js
+++ servise/schemas/users.js
@@ -1,8 +1,7 @@
 const { Schema, model } = require("mongoose");
-
+const gravatar = require("gravatar");
 const emailRegexp =
   /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/;
-
 const user = new Schema(
   {
     password: {
@@ -20,6 +19,15 @@ const user = new Schema(
       type: String,
       enum: ["starter", "pro", "business"],
       default: "starter",
+    },
+    avatarURL: {
+      type: String,
+      // required: true,
+      default() {
+        return gravatar.url(this.get("email"));
+      },
+      // default: gravatar.url(email),
+      // default: gravatar.url(this.email),
     },
     token: {
       type: String,
